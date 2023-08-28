f3226d2712b903f7b0a8709c1c6d8da39bdd11a9
Luis00809 <davidcarvajal008@gmail.com>
2023-08-27 22:11:24 -0700

    post route works

--- server.js
+++ server.js
@@ -11,7 +11,54 @@ app.use(express.urlencoded({ extended: true}));
 
 app.use(express.static('public'));
 
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/db/db.json'));
+    res.json(`${req.method} request received to get reviews`);
+    console.info(`${req.method} request has been received to get notes`);
 
+})
+
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to add a review`);
+
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title, 
+            text,
+        };
+
+        const noteString = JSON.stringify(newNote);
+
+        fs.readFile('./Develop/db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                const parsedNote = JSON.parse(data);
+                parsedNote.push(newNote);
+
+
+                fs.writeFile('./Develop/db/db.json', JSON.stringify(parsedNote, null, 4), (err) => 
+                    err
+                    ? console.error(err)
+                    : console.log(
+                        `Note for ${newNote.title} has been written to JSON file`
+                        )
+                );
+            }
+        })
+        const response = {
+            status: 'success',
+            body: newNote,
+          };
+
+          console.log(response);
+          res.status(201).json(response);
+        } else {
+          res.status(500).json('Error in posting review');
+        }
+})
 
 
 
